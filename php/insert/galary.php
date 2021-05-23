105f9f196ad04d9d4ca1a944fc39a9b877f18792
1BlackSmith <1BlackSmith.work@gmail.com>
2021-05-23 23:22:34 +0300

    fix photo download bag

--- php/insert/galary.php
+++ php/insert/galary.php
@@ -1,7 +1,9 @@
 <?php
 include_once $_SERVER['DOCUMENT_ROOT'].'/php/db_connect.php';
 
-$path = $_SERVER['DOCUMENT_ROOT'] . '/galary/img/' . $_FILES['photo'];
-if(move_uploaded_file($_FILES['photo']['typename'], $path)){
-	$result = $mysqli->query ("INSERT INTO `gallary` (`id`, `photo`) VALUES (NULL, '$path')");
+if (!empty($_FILES)) {
+    $path = $_SERVER['DOCUMENT_ROOT'] . '/galary/img/' . $_FILES['photo']['name'];
+    if (move_uploaded_file($_FILES['photo']['tmp_name'], $path)) {
+        $result = $mysqli->query("INSERT INTO `gallary` (`id`, `photo`) VALUES (NULL, '$path')");
+    }
 }
